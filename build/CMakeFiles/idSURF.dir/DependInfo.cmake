6ed56845507ff81f304cea44f96fa3368dd58f18
Thomas Funck <tfunck@ace-ws-54.(none)>
2016-08-02 17:49:13 -0400

    Updated CMakeFile.txt file so that it compiles minc_helper and idSURF. idSURF installs but doesn't link to the library correctly

---
+++ build/CMakeFiles/idSURF.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/users/tfunck/PhD/src/idSURF/gaussianiir3d.c" "/home/users/tfunck/PhD/build/CMakeFiles/idSURF.dir/src/idSURF/gaussianiir3d.c.o"
+  "/home/users/tfunck/PhD/src/idSURF/idSURF-4.2.c" "/home/users/tfunck/PhD/build/CMakeFiles/idSURF.dir/src/idSURF/idSURF-4.2.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/users/tfunck/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/users/tfunck/PhD/build/CMakeFiles/minc_helper.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
